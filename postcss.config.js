782bc2641dfd7c497d95c47c33acac29ecc7092b
Walpurga03 <Walpurga03@gmx.at>
2026-01-31 18:38:32 +0100

    Fix CI build: disable ESLint warnings and preserve CSS calc functions

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,11 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? {
+      'postcss-calc': {
+        preserve: true,
+        warnWhenCannotResolve: false
+      }
+    } : {})
   },
 }
